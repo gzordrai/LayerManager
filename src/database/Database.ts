0ba6a0ffcba0b8feecfa3e1b73c12bf0cbd0a507
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-08 01:28:55 +0200

    Add getAll function

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -22,8 +22,29 @@ export class Database {
      * 
      * @param layer The layer to remove
      */
-    public static async removeLayer(layer: Layer): Promise<void> {
-        await Database.data.delete(`/${layer.getName()}`);
+    public static async removeLayer(name: string): Promise<void> {
+        await Database.data.delete(`/${name}`);
+    }
+
+    /**
+     * Returns all the layers in the database
+     * 
+     * @returns {Promise<Array<Layer>>} All layers in the database
+     */
+    public static async getAll(): Promise<Array<Layer>> {
+        const data: any = await Database.data.getData("/");
+        let ret: Array<Layer> = new Array<Layer>();
+
+        for (const key in data) {
+            const name: string = key;
+            const locked: boolean = data[key].locked;
+            const lockerId: string = data[key].lockerId;
+            const layer: Layer = new Layer(name, locked, lockerId);
+
+            ret.push(layer);
+        }
+
+        return ret;
     }
 
     /**
