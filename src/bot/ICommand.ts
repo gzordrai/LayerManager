58de50462601f3e3466f06b9ec62e796db0068bf
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-08 01:25:59 +0200

    Change autocomplete function parameters

--- src/bot/ICommand.ts
+++ src/bot/ICommand.ts
@@ -1,7 +1,7 @@
-import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import { ApplicationCommandOptionChoiceData, AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
 
 export interface ICommand {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
-    autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
+    autocomplete?: (interaction: AutocompleteInteraction, choices: ApplicationCommandOptionChoiceData<string | number>[]) => Promise<void>;
     execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
