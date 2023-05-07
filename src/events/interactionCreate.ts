8cc86f58eb274a017d39b20856f96dd3c7364014
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-07 20:08:08 +0200

    Add bot events

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,18 @@
+import { Events, Interaction } from "discord.js";
+import { ExtendedClient, IEvent } from "../bot";
+import { handleAutocomplete, handleSlashCommand } from "../handlers"
+
+const event: IEvent = {
+    name: Events.InteractionCreate,
+    once: false,
+    async execute(client: ExtendedClient, interaction: Interaction): Promise<void> {
+        if (interaction.inCachedGuild()) {
+            if (interaction.isAutocomplete())
+                await handleAutocomplete(client, interaction);
+            else if (interaction.isChatInputCommand())
+                await handleSlashCommand(client, interaction);
+        }
+    },
+};
+
+export default event;
