0618670d70c30b6ae343d8de9bd2dd5c6be22515
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-07 20:09:52 +0200

    Add autocomplete function for some commands

--- src/bot/ICommand.ts
+++ src/bot/ICommand.ts
@@ -1,6 +1,7 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
 
 export interface ICommand {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
+    autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
     execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
