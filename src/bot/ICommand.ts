5f9150901d3ba4bd586ff6d237c615f601de61d4
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-08 01:32:43 +0200

    Put choices creation in the handler to avoid code duplication

--- src/bot/ICommand.ts
+++ src/bot/ICommand.ts
@@ -2,6 +2,5 @@ import { ApplicationCommandOptionChoiceData, AutocompleteInteraction, ChatInputC
 
 export interface ICommand {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
-    autocomplete?: (interaction: AutocompleteInteraction, choices: ApplicationCommandOptionChoiceData<string | number>[]) => Promise<void>;
     execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
--- src/handlers/autocomplete.ts
+++ src/handlers/autocomplete.ts
@@ -1,12 +1,19 @@
-import { AutocompleteInteraction } from "discord.js";
+import { ApplicationCommandOptionChoiceData, AutocompleteInteraction } from "discord.js";
 import { ExtendedClient, ICommand } from "../bot";
+import { Database, Layer } from "../database";
 
 export const handleAutocomplete = async (client: ExtendedClient, interaction: AutocompleteInteraction): Promise<void> => {
     const command: ICommand = client.commands.get(interaction.commandName)!;
 
     try {
-        if (command.autocomplete)
-            await command.autocomplete(interaction);
+        const layers: Array<Layer> = await Database.getAll();
+        let choices: ApplicationCommandOptionChoiceData<string | number>[] = new Array<ApplicationCommandOptionChoiceData<string | number>>;
+
+        layers.forEach(layer => {
+            choices.push({ name: layer.getName(), value: layer.getName() });
+        })
+        
+        await interaction.respond(choices);
     } catch (error) {
         console.log(error);
     }
