dccd5c3ef0c8eeab3e482e006b9bf4a65c43898b
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-08 20:38:23 +0200

    Revert deleting autcomplete function

--- src/handlers/autocomplete.ts
+++ src/handlers/autocomplete.ts
@@ -1,19 +1,12 @@
-import { ApplicationCommandOptionChoiceData, AutocompleteInteraction } from "discord.js";
+import { AutocompleteInteraction } from "discord.js";
 import { ExtendedClient, ICommand } from "../bot";
-import { Database, Layer } from "../database";
 
 export const handleAutocomplete = async (client: ExtendedClient, interaction: AutocompleteInteraction): Promise<void> => {
     const command: ICommand = client.commands.get(interaction.commandName)!;
 
     try {
-        const layers: Array<Layer> = await Database.getAll();
-        let choices: ApplicationCommandOptionChoiceData<string | number>[] = new Array<ApplicationCommandOptionChoiceData<string | number>>;
-
-        layers.forEach(layer => {
-            choices.push({ name: layer.getName(), value: layer.getName() });
-        })
-        
-        await interaction.respond(choices);
+        if (command.autocomplete)
+            await command.autocomplete(interaction);
     } catch (error) {
         console.log(error);
     }
