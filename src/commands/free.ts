ff54541795ea29144256e9b1cc4234e8ef795ce9
gzordrai <40698142+gzordrai@users.noreply.github.com>
2023-05-13 23:35:46 +0200

    Add a subcommand to unlock all layers

--- src/commands/free.ts
+++ src/commands/free.ts
@@ -5,12 +5,22 @@ import { Database, Layer } from "../database";
 export const command: ICommand = {
     data: new SlashCommandBuilder()
         .setName("free")
-        .setDescription("Unlock a layer")
-        .addStringOption(option =>
-            option.setName("name")
-                .setDescription("The layer name")
-                .setAutocomplete(true)
-                .setRequired(true)
+        .setDescription("Unlock one or all layers")
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("layer")
+                .setDescription("Unlock a layer")
+                .addStringOption(option =>
+                    option.setName("name")
+                        .setDescription("The layer name")
+                        .setAutocomplete(true)
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("all")
+                .setDescription("Unlock all layers")
         ),
     ephemeral: true,
     async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
@@ -25,21 +35,38 @@ export const command: ICommand = {
         await interaction.respond(ret);
     },
     async execute(interaction: ChatInputCommandInteraction, _: ExtendedClient): Promise<void> {
-        const name: string = interaction.options.getString("name", true);
+        const subcommand: string = interaction.options.getSubcommand(true);
 
-        if (await Database.has(name)) {
-            const layer: Layer = await Database.getLayer(name);
+        switch(subcommand) {
+            case "all":
+                const layers: Array<Layer> = await Database.getAll();
 
-            if (layer.islocked()) {
-                layer.unlock();
-                layer.setLockerId("");
+                layers.forEach(async (layer: Layer) => {
+                    layer.unlock();
+                    await Database.save(layer);
+                })
 
-                await Database.save(layer);
-                await interaction.followUp({ content: "The layer has been unlocked" });
-            } else
-                await interaction.followUp({ content: "This layer is not lock" });
-        } else
-            await interaction.followUp({ content: "There is no layer with this name" });
+                await interaction.followUp({ content: "All layers have been unlock" });
+                break;
+            case "layer":
+                const name: string = interaction.options.getString("name", true);
+
+
+                if (await Database.has(name)) {
+                    const layer: Layer = await Database.getLayer(name);
+
+                    if (layer.islocked()) {
+                        layer.unlock();
+                        layer.setLockerId("");
+
+                        await Database.save(layer);
+                        await interaction.followUp({ content: "The layer has been unlocked" });
+                    } else
+                        await interaction.followUp({ content: "This layer is not lock" });
+                } else
+                    await interaction.followUp({ content: "There is no layer with this name" });
+                break;
+        }
     }
 }
 
